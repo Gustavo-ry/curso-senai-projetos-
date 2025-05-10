2c94b406cd88bc5d53a282d5ea9991458755c536
Gustavo-ry <gustavoryan702@gmail.com>
2025-05-10 15:34:01 -0300

    tarefas de aprendizagem

---
+++ Gustavo ryan-python/exercicio.py
@@ -0,0 +1,17 @@
+# conversao de dolar para real
+# entrada de dados
+print("sistemas de conversao de dolar para real")
+print("desenvolvido por gustavo")
+print(" ")
+nome = input("digite seu nome")
+print(f'ola boa tarde {nome}')
+print(f'lembrando que um dolar no dia de hoje vale 5 reais')
+valor = float(input("digite o valor em dolar que deseja converter"))
+valor = valor * 5
+soma = valor * 5
+print(f'o valor em real é {valor}')
+
+
+
+
+              
