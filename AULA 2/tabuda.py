b66437722454595b417ac6afb22725fc4ec59c9d
Gustavo-ry <gustavoryan702@gmail.com>
2025-05-19 09:11:38 -0300

    Update tabuda.py

--- AULA 2/tabuda.py
+++ AULA 2/tabuda.py
@@ -6,7 +6,7 @@ numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
 for numero in numeros:
     print(f'{tabuada} x {numero} = {tabuada * numero}') 
 
-for tabuada in [2, 3, 4, 5, 6, 7, 8, 9, 10]:
+for tabuada in range(2,11,1):
     print(f'tabuada do {tabuada}')
     for numero in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
         print(f'{tabuada} x {numero} = {tabuada * numero}') 
